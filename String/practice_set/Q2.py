d49fac13e3e2c8ef47cdafa2294c0957e93b147f
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-07-29 00:51:18 +0530

    Practice string questions

---
+++ String/practice_set/Q2.py
@@ -0,0 +1,6 @@
+# Remove all spaces
+text = "I Love Python"
+print(text.replace(" ", ""))
+
+res = "".join(text.split())
+print(res)
---
+++ String/slicing.py
@@ -0,0 +1,8 @@
+text = "abcdefghijklmnopqrstuvwxyz"
+print(text[0 : 6]) # print from 0 to 5 where 6 is not exclude
+print(text[3 : 6]) # print from 3 to 5 where 6 is not exclude
+print(text[0: ]) # print from 0 to the len of the string
+print(text[ : 9]) # print from 0 to 8 where 9 is not exclude
+
+# Iterate from 3 to 19 by jump of 3
+print(text[3 : 20: 3]) # Output -> dgjmps
