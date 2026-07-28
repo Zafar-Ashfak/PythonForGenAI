585bb9e1948ed629acb2056be0808e530cd1a2c8
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-07-28 18:27:37 +0530

    Implement user input addition example

---
+++ Variables/add.py
@@ -0,0 +1,11 @@
+# a = 10
+#
+# b = 20
+#
+# print(a + b)
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+res = num1 + num2
+
+print(f"{num1} + {num2} = {res}")
