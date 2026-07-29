bf145507b50f116f095c6d5948224cc4bce04085
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-07-29 13:26:16 +0530

    Practice list questions

--- List/basics.py
+++ List/basics.py
@@ -4,43 +4,45 @@ print(my_list, type(my_list))
 
 #   ***************** LISTS METHODS *******************
 
-num = [71, 22, 30, 64, 15]
-print(num)
+nums = [71, 22, 30, 64, 15]
+print(nums)
 
 # 1. append() -> Add val to the end of the list
-num.append(56)
-print(num)
+nums.append(56)
+print(nums)
 
 # 2. extend() -> Add all items from an iterable
-num.extend([27, 38, 99])
-print(num)
+nums.extend([27, 38, 99])
+print(nums)
 
 # 3. insert() -> insert val at given position
-num.insert(3, 990)
-print(num)
+nums.insert(3, 990)
+print(nums)
 
 # 4. remove() -> Remove the first occurrence from the list
-num.remove(64)
-print(num)
+nums.remove(64)
+print(nums)
 
 # 5. pop() -> Remove and return item at index i (last item by default)
-num.pop(2)
-print(num)
+nums.pop(2)
+print(nums)
 
 # 6. count() -> Return number of occurrences from the list
 occ = 71
-print(f"{occ} occurs {num.count(occ)} times")
+print(f"{occ} occurs {nums.count(occ)} times")
 
 # 7. sort() -> Sort the list in place
-num.sort()
-print(num)
+nums.sort()
+print(nums)
 
 # 8. reverse() -> reverse the list
-num.reverse()
-print(num)
+nums.reverse()
+print(nums)
 
 # 9. copy() -> returns a shallow copy of the list
-copy_list = num.copy()
+copy_list = nums.copy()
+copy_list[2] = 1999
+print(nums)
 print(copy_list)
 
 # 10. clear() -> Removes all elements from the list
@@ -48,7 +50,7 @@ copy_list.clear()
 print(copy_list)
 
 # 11. len() -> Returns the length of the list
-print(len(num))
+print(len(nums))
 
 # 12. max() -> Returns the largest val
 print(max(2, 13, 21, 6, 19, 7))
@@ -58,7 +60,7 @@ print(min(2, 13, 21, 6, 19, 7))
 
 # 14. sum() -> Returns the sum of the given numbers
 print(sum({2, 13, 21, 6, 19, 7}))
-print(sum(num))
+print(sum(nums))
 
 # 15. index() -> Returns the index of the first occurrence
 idx = my_list.index(331)
---
+++ List/practice_set/Q1.py
@@ -0,0 +1,5 @@
+# Write a Python program to print the first, last, and middle element of a list.
+nums = [6, 2, 4, 9, 3, 5, 1, 8, 7]
+print(f"First element of the list is: {nums[0]}")
+print(f"Last element of the list is: {nums[len(nums) - 1]}")
+print(f"Middle element of the list is: {nums[len(nums) // 2]}")
