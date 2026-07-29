6bd26a4ee0b1f75b15062c11cd0a2dae2d39e3e2
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-07-29 13:03:47 +0530

    Practice string questions

--- String/slicing.py
+++ String/slicing.py
@@ -3,6 +3,7 @@ print(text[0 : 6]) # print from 0 to 5 where 6 is not exclude
 print(text[3 : 6]) # print from 3 to 5 where 6 is not exclude
 print(text[0: ]) # print from 0 to the len of the string
 print(text[ : 9]) # print from 0 to 8 where 9 is not exclude
+print(text[::-1]) # Reverse the String
 
 # Iterate from 3 to 19 by jump of 3
 print(text[3 : 20: 3]) # Output -> dgjmps
