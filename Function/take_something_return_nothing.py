07d7cb36c37416a4db16647fe6a8ef6afa476fa8
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-07-30 13:22:14 +0530

    Implement take something return nothing function

---
+++ Function/take_something_return_nothing.py
@@ -0,0 +1,14 @@
+# Write a function that takes parameters but doesn't return any value.
+
+# Function definition
+def division(num1, num2):
+    res = num1 / num2
+    print(f"{num1} / {num2} = {res}")
+
+
+def main(): # main function definition
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    division(num1, num2) # function call
+
+main() # main function call
