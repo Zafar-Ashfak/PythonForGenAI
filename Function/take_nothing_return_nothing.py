556dc48b5926e5397025d33371492002d604438c
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-07-30 13:14:58 +0530

    Implement take nothing return nothing function

---
+++ Function/take_nothing_return_nothing.py
@@ -0,0 +1,15 @@
+# Write a function that neither takes any parameters nor returns any value.
+
+# Function definition
+def addition():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    print(f"{num1} + {num2} = {num1 + num2}")
+
+
+
+def main(): #  main function definition
+    addition() # function call
+
+
+main()  # main function call
