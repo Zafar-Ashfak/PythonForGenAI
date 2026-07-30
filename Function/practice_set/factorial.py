d27f10e86ac3366592c7d0e949b1507672bb2c48
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-07-30 16:31:38 +0530

    Practice Python functions with basic algorithms

---
+++ Function/practice_set/factorial.py
@@ -0,0 +1,17 @@
+# Write a function to check factorial of a number given by user
+
+def factorial(num):
+    fac = 1
+
+    for i in range(1, num + 1):
+        fac *= i
+
+    return fac
+
+def main():
+    print("Program to print factorial of a number")
+    num = int(input("Enter a natural number: "))
+    res = factorial(num)
+    print(f"{num}! = {res}")
+
+main()
