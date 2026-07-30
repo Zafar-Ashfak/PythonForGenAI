606bdfcf18bab0b44870210edce7fe87a18b7350
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-07-30 16:22:04 +0530

    Implement is prime checker

---
+++ Function/practice_set/is_prime.py
@@ -0,0 +1,22 @@
+# Write a function to check if a number is prime or not
+
+def isprime(num):
+    if num < 2:
+        return False
+
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+
+    return True
+
+
+def main():
+    num = int(input("Enter a natural number: "))
+    res = isprime(num)
+    if res:
+        print(f"{num} is a prime number")
+    else:
+        print(f"{num} is not a prime number")
+
+main()
